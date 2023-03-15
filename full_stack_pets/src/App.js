8e81e97bab795b50e92e10526b61d4762b593be8
HelloMati <118129617+HelloMati@users.noreply.github.com>
2023-03-15 17:47:35 +0000

    initial commit

---
+++ full_stack_pets/src/App.js
@@ -0,0 +1,10 @@
+import PetsContainer from './containers/PetsContainer';
+import './App.css';
+
+function App() {
+  return (
+    <PetsContainer />
+  );
+}
+
+export default App;
---
+++ full_stack_pets/src/containers/PetsContainer.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import PetsList from "../components/PetsList";
+import PetsForm from "../components/PetsForm";
+
+const PetsContainer = () => {
+
+    const [pets, setPets] = useState([]);
+
+    const loadPetsData = async () => {
+        const response = await fetch("http://localhost:8080/pets");
+        const data = await response.json();
+        setPets(data);
+    }
+
+    useEffect(() => {
+        loadPetsData();
+    }, []);
+
+    const postPets= async (newPets) => {
+        const response = await fetch("http://localhost:8080/pets", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newPets)
+        });
+        const savedPets = await response.json();
+        setPets([...pets, savedPets]);
+    }
+
+    const deletePets = async (id) => {
+        await fetch(`http://localhost:8080/pets/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        loadPetsData();
+    }
+
+
+
+    return(
+        <>
+            <PetsForm 
+                onSubmit={postPets}
+            />
+            <PetsList 
+                pets={pets}
+                onDelete={deletePets}
+            />
+        </>
+    )
+
+}
+
+export default PetsContainer;
